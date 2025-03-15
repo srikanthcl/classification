3f81348be8024494a9469f8e650d733cb8681cb1
srikanthcl <srikanthcl@gmail.com>
2025-03-15 15:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import joblib
 import numpy as np
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Load trained model
 model = joblib.load("model.pkl")
 
 app = FastAPI()
 
+# Define request body format
+class InputData(BaseModel):
+    features: list
+
 @app.get("/")
 def home():
     return {"message": "Model API is running!"}
 
 @app.post("/predict")
-def predict(features: list):
-    prediction = model.predict([np.array(features)])
+def predict(data: InputData):
+    prediction = model.predict([np.array(data.features)])
     return {"prediction": int(prediction[0])}
